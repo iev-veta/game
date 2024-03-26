202d456b00b0d582af854945ffd7a418310b8755
capricorndecr <141959580+capricorndecr@users.noreply.github.com>
2024-03-27 01:54:33 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+
+    name="break_out",
+    version="0.1",
+    packages = find_packages(),
+    install_requirements = requirements
+
+)
