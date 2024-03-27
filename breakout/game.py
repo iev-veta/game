c1a2a465a0c25b82a1d403f46f4139845e310ed4
iev-veta <154278144+iev-veta@users.noreply.github.com>
2024-03-27 09:33:01 +0300

    Update game.py

--- breakout/game.py
+++ breakout/game.py
@@ -10,37 +10,45 @@ from paddle import Paddle
 
 class Game:
    def __init__(self):
-       pg.init()  # инициализация pygame
-       self.WIDTH = 1200  # ширина
-       self.HEIGHT = 700  # высота
-       self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))  # поле окна
-       pg.display.set_caption('Breakout')  # название окна
-       self.paddle = Paddle(self)  # создание поля подушки
-       self.ball = Ball(self)  # создание поля мячика
+      """
+      Конструктор
+      """
+      pg.init()  # инициализация pygame
+      self.WIDTH = 1200  # ширина
+      self.HEIGHT = 700  # высота
+      self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))  # поле окна
+      pg.display.set_caption('Breakout')  # название окна
+      self.paddle = Paddle(self)  # создание поля подушки
+      self.ball = Ball(self)  # создание поля мячика
 
-       self.FPS = 60  # FPS
-       self.clock = pg.time.Clock()  # поле счётчика времени
+      self.FPS = 60  # FPS
+      self.clock = pg.time.Clock()  # поле счётчика времени
 
-       self.image = pg.transform.scale(pg.image.load('bg.jpg').convert(),
+      self.image = pg.transform.scale(pg.image.load('bg.jpg').convert(),
                                         (self.WIDTH, self.HEIGHT))
 
-        self.block_list = [pg.Rect(10 + 120 * i,
+      self.block_list = [pg.Rect(10 + 120 * i,
                                    10 + 70 * j,
                                    100,
                                    50) for i in range(10) for j in range(4)]
 
-        self.color_list = [(randrange(30, 256),
+      self.color_list = [(randrange(30, 256),
                             randrange(30, 256),
                             randrange(30, 256)) for _ in range(10) for _ in range(4)]
 
    def update(self):
-       self.paddle.update()
-       self.ball.update()
-       pg.display.flip()
-       self.clock.tick(self.FPS)
+      """
+      Метод обновление
+      """
+      self.paddle.update()
+      self.ball.update()
+      pg.display.flip()
+      self.clock.tick(self.FPS)
 
    def draw(self):
-    
+      """
+      Метод отрисовки
+      """
       self.screen.blit(self.image, (0, 0))
 
       for color, block in enumerate(self.block_list):
@@ -51,15 +59,21 @@ class Game:
 
    @staticmethod
    def check_events():
-       for event in pg.event.get():
-           if event.type == pg.QUIT:
-               pg.quit()
-               sys.exit()
+      """
+      Проверка
+      """
+      for event in pg.event.get():
+         if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
 
    def run(self):
-       while True:
-           self.check_events()
-           self.update()
-           self.draw()
+      """
+      Метод запуска 
+      """
+      while True:
+         self.check_events()
+         self.update()
+         self.draw()
 
 
