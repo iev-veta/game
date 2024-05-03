97a82fd85fc0720b35dac8ffa7bb51ef039347b7
capricorndecr <141959580+capricorndecr@users.noreply.github.com>
2024-05-03 23:25:23 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 source breakout/.venv/bin/activate
 source breakout/.venv/Scripts/activate
-source breakout/.venv\Scripts\activate
-breakout/.venv/Scripts/activate
-breakout/.venv\Scripts\activate
 python3 setup.py sdist
 pip3 install .
