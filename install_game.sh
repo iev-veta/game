12ed6e69c61ffe92f0f71733f2f529fdfe937e00
iev-veta <154278144+iev-veta@users.noreply.github.com>
2024-05-06 01:01:43 +0300

    Update install_game.sh

--- install_game.sh
+++ install_game.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-python -m venv breakout/.venv
+python3 -m venv breakout/.venv
 source breakout/.venv/bin/activate
-source breakout/.venv/Scripts/activate
 python3 setup.py sdist
 pip3 install .
